4415af2483df3cafed2f8e85c32e51f2634f978b
Icaro-Lima <icaro.lima@wildlifestudios.com>
2020-05-26 01:18:47 -0300

    Heatmap structure + New

--- heatmap.go
+++ heatmap.go
@@ -1,5 +1,14 @@
 package heatmap
 
-func HelloWorld() {
-	println("Hello, World!")
+type Heatmap struct {
+	Buffer []float32
+	Max    float32
+	Width  int
+	Height int
+}
+
+func (h *Heatmap) New(width, height int) {
+	h.Width = width
+	h.Height = height
+	h.Buffer = make([]float32, width*height)
 }
